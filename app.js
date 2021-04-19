ce752311a3117ec34716dc00e956afaed575bfc1
Wentao Shi <wshi6@lsu.edu>
2021-04-19 13:53:46 -0500

    session setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require("mongoose");
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 
 const campgrounds = require('./routes/campgrounds')
@@ -38,6 +39,18 @@ app.use(methodOverride('_method'));
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'blahblahblahblah',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 // the campground routes
 app.use('/campgrounds', campgrounds);
